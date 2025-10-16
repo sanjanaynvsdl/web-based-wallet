f7b8185d382e87b2137b3f4801c50517f15ad637
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2025-10-16 16:55:40 +0530

    removed footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           <main className="flex-1">
             {children}
           </main>
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </body>
     </html>
